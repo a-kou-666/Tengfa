7dd76bdb354bf44704162aae26fd7041a2814bed
superkj <2480363497@qq.com>
2022-11-01 22:33:10 +0800

    addlist

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,9 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    num:5,
-     arr:[1,2,3,4,5,6,7,8,9],
-     imgs:["https://img.wumaow.org/upload/tu/114568731.jpg","https://img.wumaow.org/upload/tu/114568735.jpg","https://img.wumaow.org/upload/tu/114568738.jpg"],
+    
      
   },
   // 事件处理函数
